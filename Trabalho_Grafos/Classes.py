0f4a1ea30c9e4fbf6fbdf43906ac435f4190fcab
otavio <otavio.resende@estudante.ufla.br>
2019-06-19 20:41:06 -0300

    Amém

--- Trabalho_Grafos/Classes.py
+++ Trabalho_Grafos/Classes.py
@@ -1,31 +1,40 @@
 import math
 
 class Node:
-     def __init__(self, x, y, Id):
+    def __init__(self, x, y, Id):
          self.coordX = x
          self.coordY = y
          self.idNode = Id
          self.visit = False
-         self.listEdge = []
+         self.region = -1
 
-     def getCoordX(self):
+    def getCoordX(self):
          return self.coordX
 
-     def getCoordY(self):
+    def getCoordY(self):
          return self.coordY
 
-     def getId(self):
-         return self.ID
+    def getId(self):
+         return self.idNode
+     
+    def getVisit(self):
+         return self.visit
+     
+    def setVisit(self, b):
+         self.visit = b   
+
+    def setRegion(self, r):
+        self.region = r
 
 class Edge:
-     def __init__(self, V1, V2, d):
+    def __init__(self, V1, V2, d):
          self.edge = (V1, V2)
          self.distance = d
 
-     def getDistance(self):
+    def getDistance(self):
          return self.distance
 
-     def getEdge(self):
+    def getEdge(self):
          return self.edge
 
 class Set:
@@ -44,28 +53,30 @@ class Set:
     def getDemand(self):
         return self.demand
 
+    def getVisit(self):
+        return self.visit
+
+    def setVisit(self, b):
+        self.visit = b
+
 class Vehicle:
     def __init__(self, c, Id):
         self.capacity = c
         self.Id = Id
+        self.rote = []
+
+    def getId(self):
+        return self.Id
 
     def setCapacity(self, quant):
         self.capacity = self.capacity - quant
 
     def getCapacity(self):
         return self.capacity
+        
 
 def calculaDistancia(x1, y1, x2, y2):
     return math.sqrt(((x2-x1)**2)+((y2-y1)**2))
-
-
-# def main():
-    # print(calculaDistancia(3,2,1,2))
-    # node = Node(3, 5, 1)
-    # print(node.coordX)
-
-
-# main()
 
 
 
--- Trabalho_Grafos/Leituraoriginal.py
+++ Trabalho_Grafos/Leituraoriginal.py
@@ -3,11 +3,11 @@ import random
 import tsp
 from matplotlib import pyplot as plt
 from os import path
-from Classes import *
+
 
 # importa grafo do arquivo filename, a vértice 1 é a garagem dos veículos
 def get_graph(filename):
-    # graph = nx.Graph()
+    graph = nx.Graph()
     graph_settings = dict()
     file_path = path.relpath(filename)
 
@@ -46,9 +46,8 @@ def get_graph(filename):
             (node_number, coord) = current_line.split(" ", 1)
             (coordx, coordy) = (coord.rstrip("\n")).split(" ")
 
-            node = Node(int(coordx), int(coordy), int(node_number))
+            graph.add_node(int(node_number), pos = (int(coordx), int(coordy)))
 
-            Nodes.append(node)
 
             new_reader_pos = data_file.tell()
             # checa se cursor se moveu para tratar EOF prematuro
@@ -57,7 +56,13 @@ def get_graph(filename):
             else:
                 last_reader_pos = new_reader_pos
 
+
         # leitura dos sets
+
+        graph_settings["SETS_SETTINGS"] = dict()
+        graph_settings["NODE_SET"] = [None] * (int(graph_settings["DIMENSION"]) + 1)
+        graph_settings["SET_COLOR"] = [None] * (int(graph_settings["SETS"]))
+        graph_settings["NODE_COLOR"] = [None] * (int(graph_settings["DIMENSION"]))
 
         current_line = data_file.readline()
         last_reader_pos = data_file.tell()
@@ -70,24 +75,18 @@ def get_graph(filename):
             (current_set, nodes_in_set) = current_line.split(" ", 1)
             nodes_in_set = nodes_in_set.split(" ")
 
-            aux = []
+            # queremos uma lista de vértices para cada set
+            graph_settings["SETS_SETTINGS"][current_set]= {"NODES": list()}
+            graph_settings["SET_COLOR"][i] = int(random.random() * 0xFFFFFF)
 
             for each_node in nodes_in_set:
-                for i in Nodes:
-                    if i.idNode == int(each_node):
-                        i.region = current_set
+                graph_settings["SETS_SETTINGS"][current_set]["NODES"].append(int(each_node))
+                # se each_node == -1, significa que já foram lidos todos desse set
                 if int(each_node) != -1:
-                    aux.append(int(each_node))
+                    graph_settings["NODE_COLOR"][(int(each_node) - 1)] = graph_settings["SET_COLOR"][i]
+                    graph_settings["NODE_SET"][int(each_node)] = int(current_set)
 
-            sett = Set(current_set, 0)
-            tam = len(aux)
-            i = 0
-            while(i < tam):
-                sett.lixt.append(aux[i])
-                i += 1
-            
-            Sets.append(sett)
-
+        
             new_reader_pos = data_file.tell()
             # checa se o cursor se moveu para tratar EOF prematuro
             if new_reader_pos == last_reader_pos:
@@ -107,9 +106,9 @@ def get_graph(filename):
 
         for i in range(int(graph_settings["SETS"])):
             current_line = data_file.readline()
-            (current_set, current_demand) = (current_line.rstrip("\n")).split(" ", 1)
 
-            Sets[i].demand = int(current_demand)
+            (current_set, current_demand) = (current_line.rstrip("\n")).split(" ", 1)
+            graph_settings["SETS_SETTINGS"][current_set]["DEMAND"] = current_demand
 
 
             new_reader_pos = data_file.tell()
@@ -120,97 +119,41 @@ def get_graph(filename):
             else:
                 last_reader_pos = new_reader_pos
 
-
+    # tam = len(Are    # tam = len(Arestas)
+    # i = 0
+    # while(i < tam):
+    #     print(Arestas[i].edge, Arestas[i].distance)
+    #     i += 1stas)
+    # i = 0
+    # while(i < tam):
+    #     print(Arestas[i].edge, Arestas[i].distance)
+    #     i += 1
 
         current_line = data_file.readline()
         if current_line != "EOF":
             raise RuntimeError("Esperado \"EOF\", mas foi lido \"{}\"".format(current_line))
-    
-        a = 0
-        for i in range(int(graph_settings["DIMENSION"])):
-            a = a + 1
-            n = 1
 
-    return graph_settings
 
-        # return (graph, graph_settings)
 
-Nodes = []
-Sets = []
-Veiculos = []
-Arestas = []
+        return (graph, graph_settings)
+
+
 
 def main():
-    graph_settings = get_graph("./data/problemas-grupo1/problema4.txt")
+    (graph, graph_settings) = get_graph("./data/problemas-grupo1/problema10.txt")
+    pos = nx.get_node_attributes(graph, "pos")
+    a = 0
+    for i in range(int(graph_settings["DIMENSION"])):
+        a = a + 1
+        n = 1
+        for i in range(int(graph_settings["DIMENSION"])):
+            if (n != a):
+                graph.add_edges_from([(a,n)])
+                n = n + 1
+    nx.draw(graph, pos, node_color = graph_settings["NODE_COLOR"])
+    plt.show()
 
-    tam = len(Nodes)
-    i = 0
-    while(i < tam):
-        print(Nodes[i].idNode, Nodes[i].coordX, Nodes[i].coordY, Nodes[i].region)
-        i += 1
+    print(graph_settings)
 
-    SetAux = sorted(Sets, key = Set.getDemand, reverse=True)
-    tam = len(SetAux)
-    i = 0
-    while(i < tam):
-        print(SetAux[i].idSet, SetAux[i].demand, SetAux[i].lixt)
-        i += 1
-
-    for i in range(int(graph_settings["VEHICLES"])):
-        Veiculos.append(Vehicle(int(graph_settings["CAPACITY"]), int(i)))
-
-    tam = len(Veiculos)
-    i = 0
-    while(i < tam):
-        print(Veiculos[i].capacity)
-        i += 1
-
-    i = 0
-    j = 0
-    tamSets = len(Sets)
-
-    while(i < int(graph_settings["DIMENSION"])):
-        j = i
-        while(j < int(graph_settings["DIMENSION"])):     
-            if i != j and Nodes[i].region != Nodes[j].region:
-                distancia = calculaDistancia(Nodes[i].coordX, Nodes[i].coordY, Nodes[j].coordX, Nodes[j].coordY)
-                aresta = Edge(Nodes[i].idNode, Nodes[j].idNode, distancia)
-                Arestas.append(aresta)
-            j += 1
-        i += 1
-
-    tam = len(Arestas)
-    i = 0
-    while(i < tam):
-        print(Arestas[i].edge, Arestas[i].distance)
-        i += 1
-
-    i = 0
-    tamV = len(Veiculos)
-    while(i < tamV):
-        for j in SetAux:
-            if(int(Veiculos[i].capacity) < j.demand):
-                break
-            if(j.visit == False):
-                Veiculos[i].rote.append(j.idSet)
-                Veiculos[i].capacity -= int(j.demand)
-                j.visit = True
-
-        for j in SetAux:
-            if(int(Veiculos[i].capacity >= j.demand and j.visit == False)):
-                Veiculos[i].rote.append(j.idSet)
-                Veiculos[i].capacity -= int(j.demand)
-                j.visit = True
-            if(int(Veiculos[i].capacity == 0)):
-                break
-
-        i += 1
-
-    
-
-    i = 0
-    while(i < tamV):
-        print(Veiculos[i].Id, Veiculos[i].rote, Veiculos[i].capacity)
-        i += 1
 
 main()
